b7dd49683cdd15137980514e3c8622fe61628f22
Trivon Punto trivon625@outlook.com <trivon625@outlook.com>
2026-04-02 21:12:47 +0200

    Add landing page with redirect for logged in users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    redirect_to menu_path if user_signed_in?
   end
 
   def menu
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
 
-  
+
   devise_for :users
   root to: "pages#home"
   get "menu", to: "pages#menu"
   get "instructions", to: "pages#instructions"
   get "profile", to: "pages#profile"
-  
+
 resources :levels, only: [:index, :show] do
-    member do 
-      get :won
-      get :lost
-    end
-
-resources :scores, only: [:create]
-
+  member do
+    get :won
+    get :lost
   end
-  
+
+  resources :scores, only: [:create]
+
+end
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -24,5 +24,5 @@ resources :scores, only: [:create]
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  
+
 end
